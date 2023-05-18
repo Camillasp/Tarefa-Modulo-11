650688d1966c20feed6afc5173e983ba9edec71f
Camilla Pacheco <camilla_pacheco_05@yahoo.com>
2023-05-18 13:26:17 -0300

    meu primeiro commit

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -6,7 +6,7 @@ context('Funcionalidade Login', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
-        cy.get('.woocomerce-from > .button').click()
+        cy.get('.woocommerce-form > .button').click()
 
         cy.get('.page-title').should('contain', 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, aluno_ebac(Não é aluno_ebac? Sair)')
@@ -16,9 +16,9 @@ context('Funcionalidade Login', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('ebac@teste.com')
         cy.get('#password').type('teste@teste')
-        cy.get('.woocomerce-from > .button').click()
+        cy.get('.woocommerce-from > .button').click()
 
-        cy.get('.woocomerce-error').should('contain', 'Endereço de e-mail desconhecido')
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido')
         
     });
 
